2dee2d2b632a058ce950d02d43a1965c8db849e0
oussamaosman02 <oussamaosman02@gmail.com>
2022-07-13 13:14:50 +0200

    arreglando algunos errores del service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -17,7 +17,7 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then(() => {
       return fetch(e.request).catch(() =>
-        caches.match("offline.html", "assets/fcb.jpg")
+        caches.match("offline.html")
       );
     })
   );
